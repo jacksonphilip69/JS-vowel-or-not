6990d613c2e9ce591b2e5ea20f9603a57eff11eb
jackson <jacksonphilip1874511@gmail.com>
2022-04-05 17:31:57 +0530

    JS vowel or consonant

---
+++ script.js
@@ -0,0 +1,19 @@
+
+    
+
+    var let = prompt('enter a word')
+    
+
+    if (let == 'a' ||
+      let == 'e' ||
+      let == 'i' ||
+      let == 'o' ||
+      let == 'u') {
+
+      console.log('your letter is a vowel');
+
+    } else {
+
+      console.log("Your letter is a consonant");
+
+    }
